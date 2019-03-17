4993250a289e5ed006117d0fd243690d0628c203
Mahdi <mkaramidarabi@gmail.com>
2019-03-17 10:31:01 +0330

    'second'

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from .models import Blog
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+]
