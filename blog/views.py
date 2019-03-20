d809662c61ae8214d8267001f9d12b105dffa3b5
mehdikdd <mkaramidarabi@gmail.com>
2019-03-20 14:09:00 +0000

    add all changes to remote

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
+import requests
 
 def allblogs(request):
+    a=requests.get('https://api.coinbase.com/v2/prices/spot?currency=USD')
     blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+    z=str(a.json()['data']['amount'])
+    return render(request, 'blog/allblogs.html', {'blogs': blogs, 'z' : z } )
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Job
+# import requests
+
 
 # Create your views here.
 def home(request):
-    jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    client_ip = request.META.get('HTTP_X_REAL_IP')
+    if client_ip == "5.113.51.249" :
+        return HttpResponse('<h1>Your Not allowed to reach out'
+                            ' this website, apologize! Your Country is fucked up!!   ' + client_ip + '</h1>')
+    else:
+        jobs = Job.objects
+        return render(request, 'jobs/home.html', {'jobs':jobs})
